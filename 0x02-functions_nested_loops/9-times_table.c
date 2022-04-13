3f72ddf73d62b0e3f4134c6d33e6f21d66939022
Zeebams <bamideleazeezat17@gmail.com>
2022-04-12 17:27:43 -0700

    task 8

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* main -  prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+	times_table();
+	return (0);
+}
