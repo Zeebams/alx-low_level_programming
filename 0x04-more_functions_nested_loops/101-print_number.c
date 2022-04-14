bbda802abb0f6c5b4c1e391084d5d82f9c78f459
Zeebams <bamideleazeezat17@gmail.com>
2022-04-14 13:38:15 -0700

    task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,71 +1,35 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * exponent - x to the power of y
- * @x: base number
- * @y: exponent
- * Description: calcuates x^y
- * Return: x^y
- *
- **/
-
-int exponent(int x, int y)
+* print_number - prints # using _putchar function
+* @n: the integer to print
+*
+* Return: void
+*/
+void print_number(int n)
 {
-	int power;
+	int copy, nth, size = 1, ones = n % 10;
 
-	power = x;
-
-	if (x == 0)
-		return (0);
-	if (y == 0)
-		return (1);
-
-	while (y >= 2)
+	n /= 10;
+	copy = n;
+	if (ones < 0)
 	{
-		power  = power * x;
-		y--;
-	}
-	return (power);
-}
-
-/**
- * print_number - print an int using only _putchar
- * @number: int to be printed by function
- *
- * Return: nothing
- **/
-
-void print_number(int number)
-{
-	int size, digit;
-	long counter, sign;
-
-	sign = 1;
-	digit = 0;
-	size = 1;
-	counter = number;
-
-	if (number < 0)
-	{
+		ones *= -1, copy *= -1, n *= -1;
 		_putchar('-');
-		sign = -1;
-		counter *= sign;
 	}
-
-	for (; counter >= 10; size++)
+	if (copy > 0)
 	{
-		counter = counter / 10;
+		while (copy / 10 != 0)
+		{
+			copy /= 10, size *= 10;
+		}
+		while (size > 0)
+		{
+			nth = n / size;
+			_putchar('0' + nth);
+			n -= nth * size;
+			size /= 10;
+		}
 	}
-
-	counter = sign * (long)number;
-
-	while (size >= 2)
-	{
-		digit = (counter / exponent(10, size - 1));
-		_putchar(digit + '0');
-		counter = counter % exponent(10, size - 1);
-		size--;
-	}
-	_putchar(counter % 10 + '0');
+	_putchar('0' + ones);
 }
